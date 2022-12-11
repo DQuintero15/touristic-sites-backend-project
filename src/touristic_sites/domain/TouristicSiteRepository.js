c523240382b54a00545060da484917a086b6f187
Daniel Quintero <quinterod164@gmail.com>
2022-12-11 11:18:08 -0500

    ci: :sparkles: Update touristic site
    update touristic site given an id

--- src/touristic_sites/domain/TouristicSiteRepository.js
+++ src/touristic_sites/domain/TouristicSiteRepository.js
@@ -3,6 +3,7 @@ class TouristicSiteRepository {
     getTouristicSiteById = async (uuid) => { }
     createTouristicSite = async (touristicSite) => { }
     deleteTouristicSiteById = async (uuid) => { }
+    updateTouristicSite = async (uuid) => { }
 }
 
 module.exports = { TouristicSiteRepository }
